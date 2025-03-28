61e04e83021ea8e616a10221d46d6c1c49da5290
Noe Priss Definel <ndefinel0@gmail.com>
2025-03-28 12:39:53 +0100

    Ajout de la fonction pour générer un mot de passe aléatoire, suivis des tableaux de charactères

---
+++ generatePassword.js
@@ -0,0 +1,17 @@
+function generatePassword = (lenght) => {
+
+    const lowerCase = Array.from({ length: 26 }, (_, i) => String.fromCharCode(97 + i));
+    const upperCase = Array.from({ length: 26 }, (_, i) => String.fromCharCode(65 + i));
+    const number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const symbol = [
+        ...Array.from({ length: 15 }, (_, i) => String.fromCharCode(33 + i)),
+        ...Array.from({ length: 7 }, (_, i) => String.fromCharCode(58 + i)),
+        ...Array.from({ length: 6 }, (_, i) => String.fromCharCode(91 + i)),
+        ...Array.from({ length: 4 }, (_, i) => String.fromCharCode(123 + i))
+    ];
+
+    const allCharacter = [...lowerCase, ...upperCase, ...number, ...symbol];
+    let password = '';
+
+    
+}
